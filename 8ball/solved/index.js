b173f18e17a72be0ba15a2d1680fc5c5b1f997e6
Alabama Whiskey <amwtablet@gmail.com>
2022-04-20 18:29:20 -0400

    added file structure and html/css general framework

---
+++ 8ball/solved/index.js
@@ -0,0 +1,4 @@
+//JAVASCRIPT CHALLENGE: Magic 8 Ball
+//All work will be completed in this file
+//An html/css framework has already been provided
+//Suggestion: open the html file in your browser before psuedocoding
